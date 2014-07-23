cfcd4453c7599e2196a8ae5c1cc9d9587d55ba67
Chyld Medford <chyld.medford@gmail.com>
2014-07-23 15:40:48 -0500

    done

--- app/models/item.js
+++ app/models/item.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var cItem = global.mongodb.collection('items');
+var _ = require('lodash');
+
 function Item(name, room, acquired, count, cost){
   this.name = name;
   this.room = room;
@@ -7,6 +10,36 @@ function Item(name, room, acquired, count, cost){
   this.count = parseInt(count);
   this.cost = parseFloat(cost);
 }
+
+Item.prototype.save = function(cb){
+  cItem.save(this, function(err, obj){
+    cb();
+  });
+};
+
+Item.prototype.value = function(){
+  return this.cost * this.count;
+};
+
+Item.find = function(query, cb){
+  cItem.find(query).toArray(function(err, items){
+    cb(items);
+  });
+};
+
+Item.value = function(query, cb){
+  Item.find(query, function(items){
+    var sum = 0;
+
+    for(var i = 0; i < items.length; i++){
+      var item = items[i];
+      item = _.create(Item.prototype, item);
+      sum += item.value();
+    }
+
+    cb(sum);
+  });
+};
 
 module.exports = Item;
 
--- test/unit/item.js
+++ test/unit/item.js
@@ -1,11 +1,27 @@
-/* global describe, it */
+/* jshint expr:true */
+/* global describe, it, before, beforeEach */
 
 'use strict';
 
 var expect = require('chai').expect;
-var Item = require('../../app/models/item');
+var connect = require('../../app/lib/mongodb');
+var Mongo = require('mongodb');
+var Item;
 
 describe('Item', function(){
+  before(function(done){
+    connect('home-inventory-test', function(){
+      Item = require('../../app/models/item');
+      done();
+    });
+  });
+
+  beforeEach(function(done){
+    global.mongodb.collection('items').remove(function(){
+      done();
+    });
+  });
+
   describe('constructor', function(){
     it('should create a new Item object', function(){
       var couch = new Item('Couch', 'Living', '3/4/2009', '2', '1100');
@@ -18,6 +34,76 @@ describe('Item', function(){
       expect(couch.count).to.equal(2);
       expect(couch.cost).to.be.a('number');
       expect(couch.cost).to.equal(1100);
+    });
+  });
+
+  describe('#save', function(){
+    it('should save an item to the mongo database', function(done){
+      var couch = new Item('Couch', 'Living', '3/4/2009', '2', '1100');
+      couch.save(function(){
+        expect(couch._id).to.be.instanceof(Mongo.ObjectID);
+        done();
+      });
+    });
+  });
+
+  describe('.find', function(){
+    it('should find all the items from the mongo database', function(done){
+      var couch = new Item('Couch', 'Living', '3/4/2009', '2', '1100');
+      couch.save(function(){
+        Item.find({}, function(items){
+          expect(items).to.have.length(1);
+          done();
+        });
+      });
+    });
+
+    it('should find specific items from the mongo database', function(done){
+      var couch = new Item('Couch', 'Living', '3/4/2009', '2', '900');
+      var chair = new Item('Chair', 'Living', '4/7/2009', '3', '250');
+      var table = new Item('Table', 'Living', '5/1/2009', '7', '700');
+
+      couch.save(function(){
+        chair.save(function(){
+          table.save(function(){
+            Item.find({name:'Chair'}, function(items){
+              expect(items).to.have.length(1);
+              expect(items[0].name).to.equal('Chair');
+              done();
+            });
+          });
+        });
+      });
+    });
+  });
+
+  describe('#value', function(){
+    it('should return the value for a particular item', function(){
+      var couch = new Item('Couch', 'Living', '3/4/2009', '2', '900');
+      var val = couch.value();
+      expect(val).to.equal(1800);
+    });
+  });
+
+  describe('.value', function(){
+    it('should return the value for a group of items', function(done){
+      var couch     = new Item('Couch',     'Living',  '3/4/2009', '2', '900');
+      var oven      = new Item('Oven',      'Kitchen', '4/7/2009', '2', '250');
+      var microwave = new Item('Microwave', 'Kitchen', '9/7/2009', '3', '445');
+      var table     = new Item('Table',     'Living',  '5/1/2009', '7', '700');
+
+      couch.save(function(){
+        oven.save(function(){
+          microwave.save(function(){
+            table.save(function(){
+              Item.value({room:'Kitchen'}, function(value){
+                expect(value).to.equal(1835);
+                done();
+              });
+            });
+          });
+        });
+      });
     });
   });
 });
